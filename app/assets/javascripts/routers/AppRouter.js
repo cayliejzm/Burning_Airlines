9988c98e5af32c8bde1856a5d871d8b9c3d089a0
jessakang <kang.jessicaa@gmail.com>
2016-06-15 20:45:50 +1000

    good ol merge conflicts

--- app/assets/javascripts/routers/AppRouter.js
+++ app/assets/javascripts/routers/AppRouter.js
@@ -2,20 +2,17 @@ var app = app || {};
 
 app.AppRouter = Backbone.Router.extend({
   routes: {
-<<<<<<< HEAD
+
     '': 'index',
     // 'flights/:id': 'showFlight'
 
-=======
-    '': 'index'
->>>>>>> 18612432e197bba527dd7d8e532282841622b2dc
   },
 
   index: function () {
     console.log( "Empty client-side URL, index method ran" );
     var appView = new app.AppView();
     appView.render();
-    
+
   }
 
 });
