21ca5291e1c300ac3faab44fcd7d63383da80991
yumimac <yumi.ko.cheers@gmail.com>
2016-06-15 17:10:15 +1000

    changed the ajax code to backbone

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,26 @@
 
 $(document).ready(function () {
+
+
   var $lucky = $("#lucky");
   var $luckyButton = $lucky.find("button");
 
   $("#uptime button").on("click", function(){
-    $.ajax({
-      url: "/flights",
-      type: "GET",
-      dataType: "JSON"
-    }).done(function (data) {
-      console.log("it worked");
-      debugger;
+    // $.ajax({
+    //   url: "/flights",
+    //   type: "GET",
+    //   dataType: "JSON"
+    // }).done(function (data) {
+    //   console.log("it worked");
+    //   result = _.where( data, {origin:"SYD"} )
+    //   debugger;
+    // data.where
+    //   $("#uptime p").prepend(  );
+    //   $("#uptime p").prepend( "<br />")
+    // })
 
-      $("#uptime p").prepend( data[0].origin );
-      $("#uptime p").prepend( "<br />")
-    })
+    var result = app.flights.where({ origin: "SYD" });
+    console.log( result );
   })
 
 });
