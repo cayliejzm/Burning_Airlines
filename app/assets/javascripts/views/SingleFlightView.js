c2c4b8a5044de3a37f4359663fed0ce2988e6e72
yumimac <yumi.ko.cheers@gmail.com>
2016-06-16 14:05:16 +1000

    third

--- app/assets/javascripts/views/SingleFlightView.js
+++ app/assets/javascripts/views/SingleFlightView.js
@@ -1,6 +1,26 @@
 var app = app || {};
 
 app.SingleFlightView = Backbone.View.extend({
+
+  events: {
+    'click .seat': 'selectSeat',
+    // 'keydown input': 'checkForEnter',
+    // 'click button': 'selectSeat',
+    // 'click #seatBoard ul li': 'test'
+  },
+
+
+  selectSeat: function () {
+
+    $('#seatBoard ul li').on("click", function(t){
+      $(this).css({"background-color": "red"})
+      debugger;
+      // console.log("Kane")
+    })
+    console.log( "A seat selected" );
+  },
+
+
   el: "#main",
 
   render: function (flight) {
@@ -8,6 +28,11 @@ app.SingleFlightView = Backbone.View.extend({
     var flightTemplate = $("#flightTemplate").html();
       this.$el.html( flightTemplate );
       this.$el.find("#flight_number").text(flight.flight_number);
+      this.$el.find("#origin").text(flight.origin);
+      this.$el.find("#destination").text(flight.destination);
+      this.$el.find("#date").text(flight.date);
+      this.$el.find("#plane").text(flight.plane);
+
   }
 
 });
