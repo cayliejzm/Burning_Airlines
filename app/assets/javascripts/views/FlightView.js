3a1bee7676aaaa4c1f865a20d54adf232f797806
jessakang <kang.jessicaa@gmail.com>
2016-06-16 15:01:19 +1000

    user signed in as

--- app/assets/javascripts/views/FlightView.js
+++ app/assets/javascripts/views/FlightView.js
@@ -1,26 +1,10 @@
 var app = app || {};
 
 app.FlightView = Backbone.View.extend({
-  el: "#main",
 
-  render: function () {
-    console.log( "App view should be rendered now" );
-    var appViewTemplate = $("#appViewTemplate").html();
-      this.$el.html( appViewTemplate );
-
-    var flightInputView = new app.FlightInputView();
-    flightInputView.render();
-
-    var searchInputView = new app.SearchInputView();
-    searchInputView.render();
-
-    var reservationInputView = new app.ReservationInputView();
-    reservationInputView.render();
-  }
-  
   tagName: 'li',
   render: function () {
-// debugger;
+
     var flight_number = this.model.get("flight_number");
     var origin = this.model.get("origin");
     var destination = this.model.get("destination");
